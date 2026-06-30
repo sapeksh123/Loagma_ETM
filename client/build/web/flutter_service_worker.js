136f1de73d0e67578e211ba4613d5b70d1756d89
sapeksh123 <sapekshvishwakarma@gmail.com>
2026-06-30 11:36:26 +0530

    build web

--- client/build/web/flutter_service_worker.js
+++ client/build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4f90c64129e88427b7e0c4d1e50cc71
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "62270f41b441b64244e7b64e614b317c",
+"flutter_bootstrap.js": "6a340e8bade2292f6b15daa51d725ab0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
